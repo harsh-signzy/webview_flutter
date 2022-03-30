cae00fb3b1c71e4340ac25f373a020418def8c1b
Harshvardhan <harshvardhan.joshi@signzy.com>
2022-03-30 16:42:58 +0530

    initial commit, code contains custom changes

---
+++ android/src/main/java/io/flutter/plugins/webviewflutter/JavaScriptInterface.java
@@ -0,0 +1,61 @@
+package io.flutter.plugins.webviewflutter;
+import android.Manifest;
+import android.app.DownloadManager;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.util.Base64;
+import android.util.Log;
+import android.webkit.JavascriptInterface;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.content.FileProvider;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static android.content.Context.DOWNLOAD_SERVICE;
+import static androidx.core.content.ContextCompat.checkSelfPermission;
+
+public class JavaScriptInterface {
+    private Context context;
+    public JavaScriptInterface(Context context) {
+        this.context = context;
+    }
+
+    public static String getBase64StringFromBlobUrl(String blobUrl) {
+
+        if(blobUrl.startsWith("blob")){
+            return "javascript: var xhr = new XMLHttpRequest();" +
+                    "xhr.open('GET', '"+ blobUrl +"', true);" +
+                    "xhr.setRequestHeader('Content-type','application/pdf');" +
+                    "xhr.responseType = 'blob';" +
+                    "xhr.onload = function(e) {" +
+                    "    if (this.status == 200) {" +
+                    "        var blobPdf = this.response;" +
+                    "        var reader = new FileReader();" +
+                    "        reader.readAsDataURL(blobPdf);" +
+                    "        reader.onloadend = function() {" +
+                    "            base64data = reader.result;" +
+                    "            ESignDownloadPath.postMessage(base64data);" +
+                    "        }" +
+                    "    }" +
+                    "};" +
+                    "xhr.send();";
+        }
+        return "javascript: console.log('It is not a Blob URL');";
+    }
+}
---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/_prasanna_05/Desktop/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/_prasanna_05/Desktop/development/flutter/.pub-cache/hosted/pub.dartlang.org/webview_flutter-0.3.15+1/example"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/Users/_prasanna_05/Desktop/development/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.3"
+export "FLUTTER_BUILD_NUMBER=1.0.3"
